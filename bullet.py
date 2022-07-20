441da0a8a63cf4aa60a077124757b63978a244a6
il-axi <106548799+il-axi@users.noreply.github.com>
2022-07-20 06:54:19 +0300

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -6,7 +6,7 @@ class Bullet(pygame.sprite.Sprite):
         """создаю пулю в позиции пушки"""
         super(Bullet, self).__init__()
         self.screen = screen
-        self.rect = pygame.Rect(0, 0, 2, 12)
+        self.rect = pygame.Rect(0, 0, 100, 12)
         self.color = 255, 125, 39
         self.speed = 4.5
         self.rect.centerx = gun.rect.centerx
